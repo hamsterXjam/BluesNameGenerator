57b3f19e9fb5055652c8eaf7b61d283736e487ba
Toby Farrand <toby.farrand@servelec.co.uk>
2019-08-16 13:52:26 +0100

    Adding in new proof of concept functionality.

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,27 @@
 // Helper functions will go here
+
+
+var testArray = [
+    "Sceptic",
+    "Unhygienic",
+    "Depressive",
+    "Blind",
+    "Deaf",
+    "Lame",
+    "Bubonic",
+    "Bilious",
+    "Coughing",
+    "Infected",
+    "Infested"
+];
+
+function randomiser()
+{
+    var output = randomiserPlus(6);
+    return output + 1;
+}
+
+function randomiserPlus(top)
+{
+    return Math.floor(Math.random() * top);
+}
